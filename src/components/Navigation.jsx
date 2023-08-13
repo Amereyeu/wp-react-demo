6311c2b1abd64f9d993a2c260de5211f6ab60050
Amerey <info@amerey.eu>
2023-08-13 16:48:27 +0200

    demo

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,13 +1,41 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { GiSun, GiMoon } from "react-icons/gi";
 
 function Navigation({ handleThemeChange, theme }) {
+  const [isShrunk, setShrunk] = useState(false);
   const [isNavOpen, setIsNavOpen] = useState(false);
 
   const toggleNavigation = () => {
     setIsNavOpen(!isNavOpen);
   };
+
+  useEffect(() => {
+    const onScroll = () => {
+      setShrunk((isShrunk) => {
+        if (
+          !isShrunk &&
+          (document.body.scrollTop > 200 ||
+            document.documentElement.scrollTop > 200)
+        ) {
+          return true;
+        }
+
+        if (
+          isShrunk &&
+          document.body.scrollTop < 4 &&
+          document.documentElement.scrollTop < 4
+        ) {
+          return false;
+        }
+
+        return isShrunk;
+      });
+    };
+
+    window.addEventListener("scroll", onScroll);
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
 
   return (
     <div className="navigation-container">
@@ -23,26 +51,28 @@ function Navigation({ handleThemeChange, theme }) {
         <div className={`navigation__menu ${isNavOpen ? "collapse" : ""}`}>
           <Link
             to="/"
-            className="navigation__menu__item"
+            className={`navigation__menu__item ${isShrunk ? "small" : ""}`}
             onClick={toggleNavigation}>
             <button aria-label="Home">Home</button>
           </Link>
 
           <Link
             to="/gallery"
-            className="navigation__menu__item"
+            className={`navigation__menu__item ${isShrunk ? "small" : ""}`}
             onClick={toggleNavigation}>
             <button aria-label="Gallery">Gallery</button>
           </Link>
 
           <Link
             to="/contact"
-            className="navigation__menu__item"
+            className={`navigation__menu__item ${isShrunk ? "small" : ""}`}
             onClick={toggleNavigation}>
             <button aria-label="Contact">Contact</button>
           </Link>
 
-          <div className="navigation__menu__item" onClick={toggleNavigation}>
+          <div
+            className={`navigation__menu__item ${isShrunk ? "small" : ""}`}
+            onClick={toggleNavigation}>
             <button onClick={handleThemeChange} aria-label="Theme Switch">
               {theme === "light" ? <GiMoon /> : <GiSun />}
             </button>
