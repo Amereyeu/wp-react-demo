e43768854c67d5db3229c2e0d1b4b8780c8486ac
Amerey <info@amerey.eu>
2023-08-16 20:50:38 +0200

    jwt

--- src/components/Posts/MainPosts.jsx
+++ src/components/Posts/MainPosts.jsx
@@ -33,7 +33,7 @@ function MainPosts() {
       .catch((err) => console.log(err));
   }
 
-  // console.log("posts:", posts);
+  console.log("posts:", posts);
 
   useEffect(() => {
     getEvents();
