aaea59175ec18e5f5ce8ec259d24d1ca23436b78
Amerey <info@amerey.eu>
2023-08-17 19:55:59 +0200

    demo

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -1,14 +1,33 @@
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { toast } from "react-toastify";
+import { ToastContainer } from "react-toastify";
+import emailjs from "emailjs-com";
 
 function Newsletter() {
-  const { register, handleSubmit, reset, errors } = useForm();
   const [email, setEmail] = useState("");
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
 
   function onSubmit(e) {
-    toast.success("Email sent", {
-      position: "bottom-center",
+    emailjs
+      .sendForm(
+        "service_dwg0yqa",
+        "template_z2f9oax",
+        "#newsletter-form",
+        "1Di9IOAL5wlbreKHz"
+      )
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => console.log(err));
+
+    toast.success("You just subscribed to our newsletter", {
+      position: "top-center",
       autoClose: 4000,
       hideProgressBar: false,
       closeOnClick: true,
@@ -17,8 +36,8 @@ function Newsletter() {
       progress: undefined,
     });
 
-    // console.log("email:", email);
-    
+    console.log("email:", email);
+
     reset();
   }
 
@@ -30,30 +49,34 @@ function Newsletter() {
         Voluptatum maxime tenetur officiis cum est doloribus. Sapiente odio quia
         tempora ipsa non sed.
       </p>
-
-      <form className="news__form" onSubmit={handleSubmit(onSubmit)}>
+      <form
+        className="news__form"
+        id="newsletter-form"
+        onSubmit={handleSubmit(onSubmit)}>
         <input
           name="email"
           type="email"
           className="news__form__input"
           placeholder="Email"
-          onChange={(e) => setEmail(e.target.value)}
-          // ref={register({
-          //   required: true,
-          //   pattern: /^([a-z\d.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/i,
-          // })}
+          onChange={(e) => setEmail(e.currentTarget.value)}
+          {...register("email", {
+            required: true,
+            pattern: /^([a-z\d.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/i,
+          })}
         />
-        {/* {errors.email && errors.email.type === "required" && (
-          <div className="validation-error">Email is required</div>
+        {errors.email && errors.email.type === "required" && (
+          <div className="validation-error">Email cannot be empty</div>
         )}
         {errors.email && errors.email.type === "pattern" && (
-          <div className="validation-error">Enter valid email</div>
-        )} */}
-
+          <div className="validation-error">
+            Insert email address in correct form
+          </div>
+        )}
         <button type="submit" className="news__form__button">
           Subscribe
         </button>
-      </form>
+      </form>{" "}
+      <ToastContainer />
     </div>
   );
 }
