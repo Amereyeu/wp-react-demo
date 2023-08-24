f985dc64ce09572d3fd33c693c3426d866eb9dcc
Amerey <info@amerey.eu>
2023-08-24 09:06:22 +0200

    demo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,12 @@
-import { FaWordpressSimple, FaReact } from "react-icons/fa";
+import {
+  FaWordpressSimple,
+  FaReact,
+  FaBehance,
+  FaCodepen,
+  FaGithub,
+  FaRegEnvelope,
+} from "react-icons/fa";
+
 function Footer() {
   const today = new Date();
 
@@ -9,6 +17,46 @@ function Footer() {
         <FaReact className="footer__logo__icon" />
       </div>
 
+      <div className="footer__social">
+        <ul>
+          <li>
+            <a
+              href="https://www.behance.net/amrey"
+              className="navigation__menu__item"
+              target="_blank">
+              <FaBehance />
+            </a>
+          </li>
+
+          <li>
+            <a
+              href="https://codepen.io/Amerey"
+              className="navigation__menu__item"
+              target="_blank">
+              <FaCodepen />
+            </a>
+          </li>
+
+          <li>
+            <a
+              href="https://github.com/Amereyeu"
+              className="navigation__menu__item"
+              target="_blank">
+              <FaGithub />
+            </a>
+          </li>
+
+          <li>
+            <a
+              href="mailto:info@amerey.eu"
+              className="navigation__menu__item"
+              target="_blank">
+              <FaRegEnvelope />
+            </a>
+          </li>
+        </ul>
+      </div>
+
       <div className="footer__copyright">
         <p>Copyright &copy;2018 - {today.getFullYear()} WP-React Demo</p>
       </div>
@@ -17,5 +65,4 @@ function Footer() {
 }
 
 export default Footer;
-
 
